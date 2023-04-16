bf441c1f2289c45ee96a6f2cf7f0b0ce28aa3ccf
Pyromatik <benni.spiele@online.de>
2023-04-16 13:02:42 +0200

    more Blocks

--- src/main/java/net/pyromatik/tutorialmod/TutorialMod.java
+++ src/main/java/net/pyromatik/tutorialmod/TutorialMod.java
@@ -6,6 +6,7 @@ import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,6 +15,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.pyromatik.tutorialmod.Item.ModCreativeModeTabs;
 import net.pyromatik.tutorialmod.Item.ModItems;
+import net.pyromatik.tutorialmod.block.ModBlocks;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -30,6 +32,7 @@ public class TutorialMod
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -50,9 +53,22 @@ public class TutorialMod
             event.accept(ModItems.RAW_BLACK_OPAL);
         }
 
+        if (event.getTab() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.Black_Opal_Block);
+            event.accept(ModBlocks.Black_Opal_Ore);
+            event.accept(ModBlocks.Deepslate_Black_Opal_Ore);
+            event.accept(ModBlocks.Endstone_Black_Opal_Ore);
+            event.accept(ModBlocks.Netherrack_Black_Opal_Ore);
+        }
+
         if (event.getTab() == ModCreativeModeTabs.TUTORIAL_TAB){
             event.accept(ModItems.BLACK_OPAL);
             event.accept(ModItems.RAW_BLACK_OPAL);
+            event.accept(ModBlocks.Black_Opal_Block);
+            event.accept(ModBlocks.Black_Opal_Ore);
+            event.accept(ModBlocks.Deepslate_Black_Opal_Ore);
+            event.accept(ModBlocks.Endstone_Black_Opal_Ore);
+            event.accept(ModBlocks.Netherrack_Black_Opal_Ore);
         }
     }
 
